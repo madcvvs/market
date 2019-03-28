04d8601fbeec1ecd4820bb1c3631d4059180463b
VictorSvanqvist <victor.svanqvist@elev.it-gymnasiet.se>
2019-03-28 13:03:48 +0100

    f i x e

--- signup.php
+++ signup.php
@@ -2,24 +2,20 @@
 include('config.php');
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-if(isset($_POST["username"]) && isset($_POST["password"])){
     $myusername = mysqli_real_escape_string($db,$_POST['username']);
     $mypassword = mysqli_real_escape_string($db,$_POST['password']);
     
-    $sql = "SELECT * FROM users WHERE username = '$myusername' and password = '$mypassword'";
+    $sqlUsername = "SELECT * FROM users WHERE username = '$myusername'";
     $result = mysqli_query($db, $sql);
     $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
     
     $count = mysqli_num_rows($result);
     
-    if ($count >= 1) {
+    if ($count > 1) {
         //session_register("myusername");
         //$_SESSION['login_user'] = $myusername;
-        echo "Logged in";
+        echo "Successfully egistered";
     } else {
-        echo "Incorrect username or password!";
-    }   
-}else{
-    echo "NO INPUTS!";
-}
+        echo "User already exists!";           
+    }
 }
