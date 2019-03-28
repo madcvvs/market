1f45f3c65ce0a89bda05a6bf44c38d34f1bb343f
VictorSvanqvist <victor.svanqvist@elev.it-gymnasiet.se>
2019-03-28 14:27:39 +0100

    resize anpassningar 1.0

--- signup.php
+++ signup.php
@@ -6,16 +6,20 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $mypassword = mysqli_real_escape_string($db,$_POST['password']);
     
     $sqlUsername = "SELECT * FROM users WHERE username = '$myusername'";
-    $result = mysqli_query($db, $sql);
+    $result = mysqli_query($db, $sqlUsername);
     $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
     
     $count = mysqli_num_rows($result);
     
-    if ($count > 1) {
+    if ($count == 0) {
         //session_register("myusername");
         //$_SESSION['login_user'] = $myusername;
-        echo "Successfully egistered";
+        $sqlRegister = "INSERT INTO users (username, password) VALUES ('$myusername', '$mypassword')";
+        $result = mysqli_query($db, $sqlRegister);
+        echo "Successfully registered";
     } else {
-        echo "User already exists!";           
+        echo "User already exists!";
     }
+
+    echo "<script> setTimeout(function(){document.location = 'index.html'}, 5000);  </script>";
 }
