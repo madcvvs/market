dcc627950b3cd60336ba79e17d7e287ffa395ee9
DrEfficient <daniel.ssk@hotmail.com>
2019-04-01 14:23:25 +0200

    timer

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 <head>
 
 	<title>Market.com</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="shortcut icon" type="image/x-icon" href="logo.png"/>
 	<link rel="stylesheet" type="text/css" href="css.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
@@ -25,11 +26,11 @@
 var overlay = document.getElementById("overlay");
 
 window.addEventListener('load', function(){
-  
-  
+
+
   setInterval(function(){ $( "#overlay" ).fadeOut(300); }, 300);
   setInterval(function(){ overlay.style.display = 'none'; }, 1000);
-  
+
 })
 
 
@@ -49,6 +50,7 @@ function showbuttons(){
 
 	<div id="header">
 		<h1><i class="far fa-money-bill-alt"></i> Market.com </h1>
+		<p id="timetosale"></p>
 		  <div class="show-login-btn"><i class="fas fa-sign-in-alt"></i> Login</div>
       <div class="show-signup-btn"><i class="fas fa-sign-in-alt"></i> Signup</div>
       <div class="show-logout-btn"><i class="fas fa-sign-in-alt"></i> Logout</div>
@@ -208,7 +210,7 @@ $('.sok-txt').keyup(function(e){
 
     var timeoutId;
     $(".shut").hover(function() {
-			
+
         if (!timeoutId) {
             timeoutId = window.setTimeout(function() {
                 timeoutId = null;
@@ -348,5 +350,37 @@ $('.sok-txt').keyup(function(e){
 
 
     </script>
+
+		<script>
+// Set the date we're counting down to
+var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Output the result in an element with id="demo"
+  document.getElementById("timetosale").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s " + "until the sale starts";
+
+  // If the count down is over, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("timetosale").innerHTML = "THE SALE HAS NOW EXPIRED";
+  }
+}, 1000);
+</script>
+
 </body>
 </html>
