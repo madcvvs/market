22d0a5645f2cb6fd295145a41569a52b6bf5e7cb
VictorSvanqvist <victor.svanqvist@elev.it-gymnasiet.se>
2019-04-04 14:04:39 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 	<title>Market.com</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="shortcut icon" type="image/x-icon" href="logo.png"/>
-	<link rel="stylesheet" type="text/css" href="css.css">
+	<link rel="stylesheet" type="text/css" href="css.css?v=2">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 </head>
