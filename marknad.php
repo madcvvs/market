ef57d9846e3fdc45fe6c35a693fa62680eeeba9f
VictorSvanqvist <victor.svanqvist@elev.it-gymnasiet.se>
2019-04-24 13:13:10 +0200

    asf

--- marknad.php
+++ marknad.php
@@ -10,6 +10,12 @@
 	<link rel="stylesheet" type="text/css" href="css.css?v=2">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+<style>
+#content{
+	height: 860px;
+}
+</style>
 </head>
 <body>
 	    <div id="overlay">
